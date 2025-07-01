6448ce0979420bf299d82b338ad951279f1c3661
Saif Morshed <saifpt2@gmail.com>
2025-07-02 03:04:02 +0600

    Revert "Register Page design Done"
    This reverts commit f3ab24cdaf3b2a37c966a6c3e5d7db8fd305a28a.

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -66,7 +66,7 @@ onMounted(() => {
 
         <CookieBanner v-if="showCookieBanner" @accept="handleAcceptCookies" @decline="handleDeclineCookies" />
 
-        <div class="min-h-screen bg-black py-5">
+        <div class="min-h-screen bg-gray-100">
     
 
             <!-- Page Content -->
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -89,7 +89,7 @@ const submit = async () => {
             {{ feedback }}
         </div>
 
-        <form @submit.prevent="submit" :class="['register-form  bg-opacity-80 p-8 rounded-lg shadow-lg max-w-2xl mx-auto', form.processing ? 'opacity-60 pointer-events-none' : '']">
+        <form @submit.prevent="submit" :class="['bg-black bg-opacity-80 p-8 rounded-lg shadow-lg max-w-2xl mx-auto border border-gray-500', form.processing ? 'opacity-60 pointer-events-none' : '']">
             <h2 class="text-3xl font-bold text-center mb-8 text-white">Rejestracja</h2>
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <div>
@@ -156,7 +156,7 @@ const submit = async () => {
                 </div>
             </div>
             <div class="flex flex-col items-center mt-8">
-                <PrimaryButton class="py-3 text-lg bg-white text-black font-bold flex items-center justify-center" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                <PrimaryButton class="w-full py-3 text-lg bg-white text-black font-bold flex items-center justify-center" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     <svg v-if="form.processing" class="animate-spin mr-2 h-5 w-5 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z"></path>
