3b38e1b83aea03ab23eaeb4fe25d19ee628cda51
Antoni Seba <antoniwoj@o2.pl>
2025-07-14 15:59:15 +0800

    okej

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,9 @@
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="/logo.png" />
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
