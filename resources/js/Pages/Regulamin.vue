8af5f9f84665ce6ed5d0a110a585dc555088f978
Antoni Seba <antoniwoj@o2.pl>
2025-07-14 17:48:54 +0800

    okej

--- resources/js/Pages/Regulamin.vue
+++ resources/js/Pages/Regulamin.vue
@@ -8,7 +8,7 @@ import MainLayout from '@/Layouts/MainLayout.vue';
       <div class="max-w-xl w-full mx-auto flex flex-col items-center">
         <h1 class="font-brandon-grotesque-black text-2xl md:text-3xl text-white mb-6 text-center leading-tight">Regulamin</h1>
         <div class="text-gray-200 text-sm md:text-base font-medium text-center leading-relaxed mb-8 max-w-xl mx-auto">
-          <p>regulamin pojawi się w krótce</p>
+          <p>regulamin pojawi się wkrótce</p>
 
         </div>
       </div>
