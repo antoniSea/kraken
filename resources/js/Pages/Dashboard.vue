1b01d040c97527be85cd33b3f117ea5c81ffee7f
Antoni Seba <antoniwoj@o2.pl>
2025-07-15 20:14:39 +0800

    okej

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -7,7 +7,7 @@ import MainLayout from '@/Layouts/MainLayout.vue';
     <div class="w-full flex flex-col items-center justify-center min-h-[70vh]">
       <div class="w-full lg:w-[800px] bg-transparent border border-white rounded-lg px-4 py-6 flex flex-col items-center text-center woshadow-lg" style="font-family: 'Brandon Grotesque', sans-serif; background-color: #1D1D1D !important; border: 4px double #fff;;" >
         <h1 class="font-brandon-grotesque-black text-xl md:text-2xl text-white mb-4">The League of Darkness</h1>
-        <p class="font-brandon-grotesque-medium text-sm md:text-base text-gray-400 mb-6">
+        <p class="font-brandon-grotesque-medium text-sm md:text-base text-gray-400 mb-6" style="font-family: 'Brandon Grotesque', sans-serif;">
           To zamknięta inicjatywa skierowana do wybranych członków społeczności barmańskiej – tych, którzy czują, że za perfekcyjnie podanym koktajlem, precyzyjnym ruchem ręki i uśmiechem
           w półmroku, kryje się coś więcej. Coś głębszego. Ciemniejszego.
         </p>
