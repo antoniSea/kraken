cb59825a4f9642c79f1d06cbb80add8f0fa4dde8
Antoni Seba <antoniwoj@o2.pl>
2025-09-15 16:11:50 +0700

    okej

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -6,7 +6,7 @@ import axios from 'axios';
 const navLinks = [
   { name: 'Historia', href: '/dashboard' },
   { name: 'Kodeks', href: '/kodeks' },
-  { name: 'Regulamin', href: '/regulamin' },
+  { name: 'Regulamin', href: '/Regulamin_The League_of_Darkness.pdf', target: '_blank' },
   { name: 'Profil', href: '/profile' },
 ];
 
@@ -101,6 +101,12 @@ onBeforeUnmount(() => {
               </div>
             </div>
           </div>
+          <a v-else-if="link.target === '_blank' && !link.disabled" :href="link.href" target="_blank" rel="noopener noreferrer" class="text-white cursor-pointer relative flex flex-col items-center px-2"
+            :class="{ 'font-bold': currentPath === link.href }">
+            {{ link.name }}
+            <span v-if="currentPath === link.href" class="block w-6 h-0.5 mt-1 bg-white rounded-full" />
+            <span v-else class="block w-6 h-0.5 mt-1 bg-white/60 rounded-full opacity-0" />
+          </a>
           <Link v-else-if="!link.disabled" :href="link.href" class="text-white cursor-pointer relative flex flex-col items-center px-2"
             :class="{ 'font-bold': currentPath === link.href }">
             {{ link.name }}
